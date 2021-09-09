c7fe9e44584ab26cfa9019fb05dd47bc9e2556eb
Michael Quilachamin <90359984+Rodion26@users.noreply.github.com>
2021-09-08 20:40:59 -0500

    Primer ejercicio hecho en clase (Script)

---
+++ Ejercicio-1.py
@@ -0,0 +1,35 @@
+print("Empezando a trabajar con Python")
+print("Realizado por: 'Michael Quilachamin'")
+#Utilizamos la función "print()" para mostrar en pantalla el texto.
+#Además, el texto debe ir entre comillas para ser reconocido como tal.
+
+print('Consultando los tipos de valores:')
+
+print("El tipo de dato de 875 es:")
+print(type(875))
+
+print("El tipo de dato de 4.89 es:")
+print(type(4.89))
+
+print("El tipo de dato del texto: 'Now is better than ever es':")
+print(type('Now is better than ever'))
+
+print("El tipo de dato de 1.32 es:")
+print(type(1.32))
+#Usamos la función "print()" para mostrar en pantalla el texto.
+#Anidamos las funciones nativas "print()" con "Type()" para mostrar en pantalla el tipo de dato identificado. 
+#Ejemplo de las funciones anidadas: "print(type())"
+
+print("¿El valor 5+8i corresponde a un valor entero?")
+print(isinstance(5+8j, int))
+
+print("¿El valor 8.2 corresponde a un valor entero?")
+print(isinstance(8.2, int))
+
+print("¿El texto 'Readability counts' corresponde a un string?")
+print(isinstance('Readability counts', str))
+
+#Usamos la función "print()" para mostrar en pantalla el texto.
+#Anidamos las funciones nativas "print()" con "isinstance()" para mostrar en pantalla la comparación entre dos datos.
+#En pantalla se mostrará el valor lógico que recibe cada comparación de datos.
+#Ejemplo de las funciones anidadas: "print(isinstance())"
